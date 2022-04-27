308c73de0c69ae5fee3faca6c2440fd77a12902f
Sukruth A V <65019778+Sukruth097@users.noreply.github.com>
2022-04-27 17:59:23 +0530

    Add files via upload

---
+++ capture_image.py
@@ -0,0 +1,28 @@
+import os
+import cv2
+import time
+import uuid
+
+IMAGE_PATH='CollectedImages'
+
+labels=['Hello','Yes','No','Thanks','IloveYou','Please']
+
+number_of_images=2
+
+for label in labels:
+    img_path = os.path.join(IMAGE_PATH, label)
+    os.makedirs(img_path,exist_ok=True)
+    cap=cv2.VideoCapture(0)
+    print('Collecting images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range(number_of_images):
+        ret,frame=cap.read()
+        imagename=os.path.join(IMAGE_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        #imagename=[os.path.join(IMAGE_PATH,label,label + '{}.jpg'.format(i)) for i in range(1, 3) ]
+        cv2.imwrite(imagename,frame)
+        cv2.imshow('frame',frame)
+        time.sleep(2)
+        
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+    cap.release()
